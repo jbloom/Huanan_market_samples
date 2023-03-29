36310d365781476f8cfafd402d29760a210d65cd
jbloom <jbloom@fredhutch.org>
2023-03-29 06:08:20 -0700

    aggregate metadata and get list of FASTQs

--- Snakefile
+++ Snakefile
@@ -9,13 +9,15 @@ configfile: "config.yaml"
 
 
 checkpoint process_metadata:
-    """Process metadata sample sheet."""
+    """Process metadata sample sheet to aggregate metadata and get list of FASTQs."""
     input:
         excel="data/CRA010170.xlsx",
     output:
         samples="results/metadata/samples.csv",
         experiments="results/metadata/experiments.csv",
         runs="results/metadata/runs.csv",
+        metadata="results/metadata/merged_metadata.csv",
+        fastqs="results/metadata/fastqs.csv",
     conda:
         "environment.yml"
     script:
--- scripts/process_metadata.py
+++ scripts/process_metadata.py
@@ -1,13 +1,116 @@
 """Process metadata from Excel."""
 
 
+import os
+import itertools
+
 import pandas as pd
 
 
-metadata = pd.read_excel(snakemake.input.excel, sheet_name=None)
+excel_metadata = pd.read_excel(snakemake.input.excel, sheet_name=None)
 
-for sheetname, sheet in metadata.items():
-    output_name = sheetname.lower() + "s"  # name we give output file in snakemake
-    output_csv = snakemake.output[output_name]
-    print(f"Writing {sheetname=} to {output_csv=}")
-    sheet.to_csv(output_csv, index=False)
+assert set(excel_metadata) == {"Sample", "Experiment", "Run"}
+
+samples = excel_metadata["Sample"]
+samples.to_csv(snakemake.output.samples, index=False)
+
+runs = excel_metadata["Run"]
+runs.to_csv(snakemake.output.runs, index=False)
+
+experiments = excel_metadata["Experiment"]
+experiments.to_csv(snakemake.output.experiments, index=False)
+
+assert set(samples["Sample name"]) == set(experiments["BioSample name"])
+assert set(samples.columns).intersection(experiments.columns) == {"Accession", "ID"}
+
+metadata = (
+    samples
+    .drop(columns="ID")
+    .rename(columns={"Accession": "BioSample accession"})
+    .merge(
+        (
+            experiments
+            .drop(columns="ID")
+            .rename(columns={"Accession": "Experiment accession"})
+        ),
+        on="BioSample accession",
+        validate="one_to_many",
+    )
+    .drop(
+        columns=[
+            "accession_in_other_db",
+            "other_db",
+            "other_db_url",
+            "BioProject accession",
+        ]
+    )
+    .merge(
+        runs.drop(columns="ID").rename(columns={"Accession": "Run accession"}),
+        on="Experiment accession",
+        validate="one_to_many",
+    )
+)
+
+# drop uninformative columns
+uninformative_columns = [
+    c for c in metadata.columns if metadata[c].nunique(dropna=False) <= 1
+]
+metadata = metadata.drop(columns=uninformative_columns)
+
+# drop duplicated columns with different names
+dup_cols = [
+    cols[1] for cols in itertools.combinations(metadata.columns, 2)
+    if (metadata[cols[0]] == metadata[cols[1]]).all()
+]
+metadata = metadata.drop(columns=dup_cols)
+
+# drop other irrelevant columns
+metadata = metadata.drop(columns=["Planned number of cycles", "Run title"])
+
+# group download paths and MD5 checksums
+metadata = (
+    metadata
+    .drop(columns=[col for col in metadata.columns if col.startswith("File name")])
+    .assign(
+        download_urls=lambda x: (
+            x[[c for c in metadata.columns if c.startswith("DownLoad")]].apply(
+                lambda vals: [val for val in vals if not pd.isnull(val)],
+                axis=1,
+            )
+        ),
+        md5_checksums=lambda x: (
+            x[[c for c in metadata.columns if c.startswith("MD5")]].apply(
+                lambda vals: [val for val in vals if not pd.isnull(val)],
+                axis=1,
+            )
+        ),
+        fastqs=lambda x: x["download_urls"].map(lambda fs: [os.path.join("results/fastqs", os.path.basename(f)) for f in fs]),
+    )
+    .drop(columns=[col for col in metadata.columns if col.startswith("DownLoad")])
+    .drop(columns=[col for col in metadata.columns if col.startswith("MD5")])
+)
+
+# get most relevant columns first
+first_cols = [
+    "Sample name",
+    "Sample title",
+    "Public description",
+    "Collection date",
+    "Run accession",
+    "BioSample accession",
+    "fastqs",
+]
+
+metadata = metadata[first_cols + [c for c in metadata.columns if c not in first_cols]]
+
+print(f"Writing merged metadata to {snakemake.output.metadata}")
+metadata.to_csv(snakemake.output.metadata, index=False)
+
+fastqs_to_get = (
+    metadata
+    [["fastqs", "download_urls", "md5_checksums"]]
+    .explode(column=["fastqs", "download_urls", "md5_checksums"])
+)
+
+print(f"Writing FASTQs to get to {snakemake.output.fastqs}")
+fastqs_to_get.to_csv(snakemake.output.fastqs, index=False)
